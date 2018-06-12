2c57da50dd4856a44b7152af50e26fa996aa1430
Via <1627973075@qq.com>
2018-06-12 16:30:21 +0800

    commit Library

--- src/main/java/com/tw/Library.java
+++ src/main/java/com/tw/Library.java
@@ -1,7 +1,131 @@
 package com.tw;
 
+import java.util.*;
+import java.util.regex.*;
+import java.util.stream.Collectors;
+
 public class Library {
-    public boolean someLibraryMethod() {
-        return true;
+
+    private List<Student> studentList;
+
+    public Library(){
+        this.studentList = new ArrayList<>();
+    }
+    public void printMainInterface() {
+        String initString = "```\n" +
+                "1. 添加学生\n" +
+                "2. 生成成绩单\n" +
+                "3. 退出\n" +
+                "请输入你的选择（1～3）：\n" +
+                "```";
+        System.out.println(initString);
+    }
+
+    public void printAddInterface(){
+        System.out.println("```\n" +
+                "请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n" +
+                "```");
+    }
+
+
+
+    public boolean matchAddInputForm(String input) {
+        String regex = "(^[\\u4E00-\\u9FFF]+)，\\d+，[\\u4E00-\\u9FFF]+：\\d{1,3}(，[\\u4E00-\\u9FFF]+：\\d{1,3})*";
+        boolean isMatch = Pattern.matches(regex, input);
+        return isMatch;
+    }
+
+    public boolean matchPringInputForm(String input) {
+        String regex = "\\d+(，\\d+)*";
+        return Pattern.matches(regex, input);
+    }
+
+    public String getInput(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public void addStudent(String description) {
+        Student student = new Student(description);
+        studentList.add(student);
+        System.out.println("```\n" +
+                "学生xxx的成绩被添加\n" +
+                "```");
+        printMainInterface();
+    }
+
+    public void printAll() {
+        float total = 0;
+        System.out.println("```\n" +
+                "成绩单\n" +
+                "姓名|数学|语文|英语|编程|平均分|总分\n" +
+                "========================");
+        for (Student stu : studentList){
+            String printLine = stu.name + "|"
+                    + stu.mathScore + "|"
+                    + stu.chiScore + "|"
+                    + stu.engScore + "|"
+                    + stu.proScore + "|"
+                    + stu.average + "|"
+                    + stu.total + "|";
+            total += stu.total;
+            System.out.println(printLine);
+        }
+        List<Student> sortStudentList = studentList.stream().sorted(new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                if (o1.total > o2.total)
+                    return 1;
+                else if (o1.total == o2.total)
+                    return 0;
+                else
+                    return -1;
+            }
+        }).collect(Collectors.toList());
+        int length = studentList.size();
+        float left = sortStudentList.get(length / 2).total;
+        float right = sortStudentList.get((length + 1) / 2).total;
+        float middle = (left + right) / 2;
+
+        System.out.println("========================");
+        System.out.println("全班总分平均数：" + total);
+        System.out.println("全班总分中位数：" + middle);
+    }
+
+    private class Student{
+        public String name;
+        public int id;
+        public float mathScore = 0;
+        public float chiScore = 0;
+        public float engScore = 0;
+        public float proScore = 0;
+        public float average = 0;
+        public float total = 0;
+
+        public Student(String toxen){
+            String[] strList = toxen.split("，");
+            this.name = strList[0];
+            this.id = Integer.valueOf(strList[1]);
+            for (int i = 2; i < strList.length; i++){
+                String[] subject_score = strList[i].split("：");
+                String subject = subject_score[0];
+                float score = Float.valueOf(subject_score[1]);
+                switch (subject){
+                    case "数学":
+                        this.mathScore = score;
+                        break;
+                    case "语文":
+                        this.chiScore = score;
+                        break;
+                    case "英语":
+                        this.engScore = score;
+                        break;
+                    case "编程":
+                        this.proScore = score;
+                }
+                this.total = mathScore + chiScore + engScore + proScore;
+                this.average = total / 4;
+            }
+        }
     }
 }
--- src/test/java/com/tw/LibraryTest.java
+++ src/test/java/com/tw/LibraryTest.java
@@ -1,19 +1,102 @@
 package com.tw;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.LinkedList;
+import java.util.Scanner;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.contains;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class LibraryTest {
+
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setup() {
+        System.setOut(new PrintStream(outContent));
+    }
+
     @Test
-    public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    public void test_print_main_interface(){
+        Library library = new Library();
+        library.printMainInterface();
+        String str = "```\n" +
+                "1. 添加学生\n" +
+                "2. 生成成绩单\n" +
+                "3. 退出\n" +
+                "请输入你的选择（1～3）：\n" +
+                "```\r\n";
+        assertThat(str, equalTo(systemOut()));
+    }
+
+    @Test
+    public void test_input_1_return_add(){
+        Library library = new Library();
+        library.printAddInterface();
+        String result = "```\n" +
+                "请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n" +
+                "```\r\n";
+        assertThat(result, equalTo(systemOut()));
+    }
+    @Test
+    public void test_match_add_input_form(){
+        Library library = new Library();
+        String input1 = "张三，123，语文：89，数学：89";
+        String input2 = "张三，语文：89，数学：89";
+
+        boolean result1 = library.matchAddInputForm(input1);
+        assertTrue(result1);
+        assertTrue(!library.matchAddInputForm(input2));
+    }
+
+    @Test
+    public void test_match_print_input_form(){
+        Library library = new Library();
+        String input1 = "123，34，453，65";
+        String input2 = "，123，34";
+
+        assertTrue(library.matchPringInputForm(input1));
+        assertTrue(!library.matchPringInputForm(input2));
+
+    }
+
+    @Test
+    public void test_add_student(){
+        Library library = new Library();
+        String student = "张三，123，语文：89，数学：89";
+
+        library.addStudent(student);
+
+        String out = "```\n" +
+                "学生xxx的成绩被添加\n" +
+                "```\r\n";
+        assertThat(systemOut(),containsString(out));
+    }
+
+    @Test
+    public void test_print_students(){
+        Library library = new Library();
+
+        String stu1 = "张三，123，语文：89，数学：78，英语：85，编程：95";
+        String stu2 = "李四，234，语文：78，数学：85，英语：84，编程：85";
+        String stu3 = "王五，345，语文：97，数学：84，英语：88，编程：92";
+        library.addStudent(stu1);
+        library.addStudent(stu2);
+        library.addStudent(stu3);
+
+        library.printAll();
+
+        String out = "成绩单";
+        assertThat(systemOut(), containsString(out));
     }
 
     @Test
@@ -27,6 +110,12 @@ public class LibraryTest {
 
         assertEquals(mockedList.get(0), value);
 
+    }
+
+
+    private String systemOut() {
+        String out = outContent.toString();
+        return out;
     }
 
 }
